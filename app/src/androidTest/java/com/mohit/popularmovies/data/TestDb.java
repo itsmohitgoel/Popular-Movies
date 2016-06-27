9a335699bae29b08c57fefa4e105086ee8965ecb
itsmohitgoel <itsmohitgoel@gmail.com>
2016-06-27 08:34:02 +0530

    feat: Complete and run basic test for movie table & DB

--- app/src/androidTest/java/com/mohit/popularmovies/data/TestDb.java
+++ app/src/androidTest/java/com/mohit/popularmovies/data/TestDb.java
@@ -1,5 +1,6 @@
 package com.mohit.popularmovies.data;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.test.AndroidTestCase;
 
@@ -8,11 +9,11 @@ import java.util.HashSet;
 /**
  * Created by Mohit on 26-06-2016.
  */
-public class TestDb extends AndroidTestCase{
+public class TestDb extends AndroidTestCase {
     public static final String LOG_TAG = TestDb.class.getSimpleName();
 
     // Since I want to start with  a clean slate
-    void deleteTheDatabase(){
+    void deleteTheDatabase() {
         mContext.deleteDatabase(MovieDbHelper.DATABASE_NAME);
     }
 
@@ -27,7 +28,7 @@ public class TestDb extends AndroidTestCase{
         This only test that the Movie table has the correct column names, regardless of the
         data it should contain.
      */
-    public void testCreateDb(){
+    public void testCreateDb() {
         // build a HashSet of all of the table names we wish to look for
         final HashSet<String> tableNameHashSet = new HashSet<String>();
         tableNameHashSet.add(MovieContract.MovieEntry.TABLE_NAME);
@@ -35,5 +36,41 @@ public class TestDb extends AndroidTestCase{
         deleteTheDatabase();
         SQLiteDatabase db = new MovieDbHelper(this.mContext).getWritableDatabase();
         assertEquals(true, db.isOpen());
+
+        // have we created  the database we want?
+        Cursor c = db.rawQuery("SELECT name from sqlite_master where type='table'", null);
+        assertTrue("Error: This means that database has not been created correctly", c.moveToFirst());
+
+        // verify that the tables have been created
+        do {
+            tableNameHashSet.remove(c.getString(0));
+        } while (c.moveToNext());
+
+        assertTrue("Error: Database was created without movie entry table", tableNameHashSet.isEmpty());
+
+        // Now, do my tables contain correct columns?
+        c = db.rawQuery("PRAGMA table_info(" + MovieContract.MovieEntry.TABLE_NAME + " )", null);
+        assertTrue("Error: This means we were unable to query the database for table information", c.moveToFirst());
+
+        // Build a HashSet of all the column names we want to look for
+        final HashSet<String> movieColumnHashSet = new HashSet<>();
+        movieColumnHashSet.add(MovieContract.MovieEntry._ID);
+        movieColumnHashSet.add(MovieContract.MovieEntry.COLUMN_ORIGINAL_TITLE);
+        movieColumnHashSet.add(MovieContract.MovieEntry.COLUMN_OVERVIEW);
+        movieColumnHashSet.add(MovieContract.MovieEntry.COLUMN_RELEASE_DATE);
+        movieColumnHashSet.add(MovieContract.MovieEntry.COLUMN_VOTE_AVERAGE);
+        movieColumnHashSet.add(MovieContract.MovieEntry.COLUMN_POSTER_PATH);
+        movieColumnHashSet.add(MovieContract.MovieEntry.COLUMN_BACKDROP_PATH);
+
+        int columnNameIndex = c.getColumnIndex("name");
+        do {
+            String columnName = c.getString(columnNameIndex);
+            movieColumnHashSet.remove(columnName);
+        } while (c.moveToNext());
+
+        //If this fails that means my database doesn't contain all of the required Movie Entrly
+        // columns
+        assertTrue("Error: The database doesn't contain all of the required movie entry columns", movieColumnHashSet.isEmpty());
+        db.close();
     }
 }
--- app/src/main/java/com/mohit/popularmovies/data/MovieDbHelper.java
+++ app/src/main/java/com/mohit/popularmovies/data/MovieDbHelper.java
@@ -28,7 +28,7 @@ public class MovieDbHelper extends SQLiteOpenHelper {
                 MovieEntry.COLUMN_RELEASE_DATE + " TEXT NOT NULL, " +
                 MovieEntry.COLUMN_VOTE_AVERAGE + " REAL NOT NULL, " +
                 MovieEntry.COLUMN_POSTER_PATH + " TEXT NOT NULL, " +
-                MovieEntry.COLUMN_BACKDROP_PATH + " TEXT NOT NULL, " +
+                MovieEntry.COLUMN_BACKDROP_PATH + " TEXT NOT NULL " +
                 " );";
         db.execSQL(SQL_CREATE_MOVIE_TABLE);
     }
