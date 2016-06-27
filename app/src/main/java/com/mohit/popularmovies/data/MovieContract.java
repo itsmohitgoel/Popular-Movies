0535b6e6d1767d077f0a387f2e2f28b67f0a3003
itsmohitgoel <itsmohitgoel@gmail.com>
2016-06-27 08:42:02 +0530

    Define TrailerEntry constants in contract

--- app/src/main/java/com/mohit/popularmovies/data/MovieContract.java
+++ app/src/main/java/com/mohit/popularmovies/data/MovieContract.java
@@ -19,4 +19,19 @@ public class MovieContract {
         public static final String COLUMN_POSTER_PATH = "poster_path";
         public static final String COLUMN_BACKDROP_PATH = "backdrop_path";
     }
+
+    /* Inner class that defines the table contents of the trailer table */
+    public static final class TrailerEntry implements BaseColumns{
+        public static final String TABLE_NAME = "trailer";
+
+        //Column with the foreign key into 'movie' table
+        public static final String COLUMN_MOVIE_ID = "movie_id";
+
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_SITE = "site";
+        public static final String COLUMN_TRAILER_KEY = "key";
+        public static final String COLUMN_SIZE = "size";
+        public static final String COLUMN_TYPE = "type";
+
+    }
 }
