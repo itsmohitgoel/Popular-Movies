7dfeef6242506646942c7c4f5fe3feb2b54e9d33
itsmohitgoel <itsmohitgoel@gmail.com>
2016-07-04 11:12:22 +0530

    Add two URIs for movie and trailer db tables

--- app/src/main/java/com/mohit/popularmovies/data/MovieContract.java
+++ app/src/main/java/com/mohit/popularmovies/data/MovieContract.java
@@ -1,5 +1,8 @@
 package com.mohit.popularmovies.data;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -8,8 +11,21 @@ import android.provider.BaseColumns;
  */
 public class MovieContract {
 
+    public static final String CONTENT_AUTHORITY = "com.mohit.popularmovies.app";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_MOVIE = MovieEntry.TABLE_NAME;
+    public static final String PATH_TRAILER = TrailerEntry.TABLE_NAME;
+
     /* Inner class that defines the table contents  of the movie table*/
     public static final class MovieEntry implements BaseColumns {
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIE).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE;;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/"  + PATH_MOVIE;
+
         public static final String TABLE_NAME = "movie";
 
         public static final String COLUMN_ORIGINAL_TITLE = "original_title";
@@ -18,10 +34,23 @@ public class MovieContract {
         public static final String COLUMN_VOTE_AVERAGE = "vote_average";
         public static final String COLUMN_POSTER_PATH = "poster_path";
         public static final String COLUMN_BACKDROP_PATH = "backdrop_path";
+
+        // Uri constructor to build for a specific movie entry
+        public static Uri buildMovieUri(long id) {
+            Uri movieUri = ContentUris.withAppendedId(CONTENT_URI, id);
+            return movieUri;
+        }
     }
 
     /* Inner class that defines the table contents of the trailer table */
-    public static final class TrailerEntry implements BaseColumns{
+    public static final class TrailerEntry implements BaseColumns {
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_TRAILER).build();
+
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE +
+                "/" + CONTENT_AUTHORITY + "/" + PATH_TRAILER;
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE +
+                "/" + CONTENT_AUTHORITY + "/" + PATH_TRAILER;
+
         public static final String TABLE_NAME = "trailer";
 
         //Column with the foreign key into 'movie' table
@@ -33,5 +62,8 @@ public class MovieContract {
         public static final String COLUMN_SIZE = "size";
         public static final String COLUMN_TYPE = "type";
 
+        public static Uri buildTrailerUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
     }
 }
