bcde61592b160c9a9fcef63ea92b57bcc0c07ad0
itsmohitgoel <itsmohitgoel@gmail.com>
2016-08-17 04:35:30 +0530

    Handle Item Selection on Table: fix: Navigate to previous running instance of MainActivity from settings activity, where any X item was selected before.

--- app/src/main/java/com/mohit/popularmovies/SettingsActivity.java
+++ app/src/main/java/com/mohit/popularmovies/SettingsActivity.java
@@ -1,5 +1,8 @@
 package com.mohit.popularmovies;
 
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
@@ -38,5 +41,11 @@ public class SettingsActivity extends PreferenceActivity implements Preference.O
             preference.setSummary(stringValue);
         }
         return true;
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    public Intent getParentActivityIntent() {
+        return super.getParentActivityIntent().addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
     }
 }
